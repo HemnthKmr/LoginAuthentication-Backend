2fc876e0d32432372f0b94167385923cd41aaaca
HEMANTH KUMAR G <HEMANTH.KUMARG@lntinfotech.com>
2021-01-11 15:06:54 -0500

    New changes added for Register and login

--- passportConfig.js
+++ passportConfig.js
@@ -6,32 +6,43 @@ const passport = require('passport');
 
 module.exports = (passport) => {
     passport.use(
-        new localStrategy((email,password,done) => {
+        new localStrategy(
+            {
+                usernameField: 'email',
+                passwordField: 'password'
+            },(email,password,done) => {
             User.findOne({ 
                 where:{email: email}
-            }).then( () => {
-                return done(null,false);
-            }).catch( (data) => {
-                bcrypt.compare(password,data.password, (err, result)=> {
-                    if(err) throw err;
-                    if(result === true){
-                        return done(null,data);
-                    }
-                    else{
-                        return done(null,false);
-                    }
-                });
-            });
-        }))
-}
+            }).then( (data) => {
+                if(data === null){
+                    return done(null,false);
+                }
+                if(data !== null)
+                {
+                    bcrypt.compare(password,data.password, (err, result)=> {
+                        if(err) return done(null, false);
+                        if(result === true){
+                            return done(null,data);
+                        }
+                        else{
+                            return done(null,false);
+                        }
+                    });
+                }
+            })
+            .catch(err => console.log(err));
+        }));
+
 
 passport.serializeUser((data,cb) => {
     cb(null,data.id);
 });
+
 passport.deserializeUser((id,cb)=> {
     User.findOne({ 
         where: {id: id}
     }).then((err,data) => {
         cb(err,data)
-    })
-})
+    });
+});
+}
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const passport = require('passport');
 const passportLocal = require('passport-local').Strategy;
 const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
 const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const db = require('./models');
@@ -21,54 +22,74 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(session({
-    secret: "secretcode",
-    resave: true,
-    saveUninitialized: true
-}));
-
 app.use(cookieParser("secretcode"));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());
 require('./passportConfig')(passport);
+
+app.use(session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+    maxAge: 3600000
+}));
 
 //Routes
 
 app.post('/login', (req, res, next) => {
-    passport.authenticate("local",(err,data) => {
-        if(err) throw err;
-        if(!data) res.send("No User Exists");
-        else{
-            req.logIn((data, err) => {
-                if(err) throw err;
-                res.send("Successfully authenticated");
-                console.log(req.data);
+    passport.authenticate("local",{
+        successRedirect: 'http://localhost:3000/dashboard',
+        failureRedirect: 'http://localhost:3000/login'
+    },
+         (err, data) => {
+        if (err) throw err;
+        if (data === null) res.send("No User Exists");
+        else {
+            req.logIn(data,() => {
+                if(data !== false){
+                    console.log("Successfully authenticated");
+                    return res.redirect("http://localhost:3000/dashboard")
+                }
+                else{
+                    console.log("password Doesnt Match");
+                    return res.redirect("http://localhost:3000/login")
+                }
             })
         }
-    }) (req,res,next);
+    })(req, res, next);
 });
 
 app.post('/register', (req, res) => {
     User.findOne({
         where: { email: req.body.email }
-    }).then(() => {
-        console.log("User already exists");
-        res.redirect("http://localhost:3000/register")
+    }).then( async (data) => {
+        if(data === null){
+                    const hashPassword = await bcrypt.hash(req.body.password, 10);
+                    const newUser = {
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: hashPassword
+                    }
+                    await User.create(newUser)
+                        .then(res.redirect("http://localhost:3000/login"))
+                        .catch((err) => console.log(err))
+                }
+        if(data !== null){
+            console.log("User already exists");
+            res.redirect("http://localhost:3000/register")
+        }
     })
-        .catch(async () => {
-            const hashPassword = await bcrypt.hash(req.body.password, 10);
-            const newUser = {
-                name: req.body.name,
-                email: req.body.email,
-                password: hashPassword
-            }
-            await User.create(newUser)
-                .then(res.redirect("http://localhost:3000/login"))
-                .catch((err) => console.log(err))
-        });
+        .catch(err => console.log(err))
 })
 
-app.get('/user', (req, res) => { res.send(req.data)})
+app.get('/dashboard/:id', (req,res) => {
+    User.findOne({
+        where: { email: req.body.email }
+}).then(res.redirect("http://localhost:3000/dashboard"))
+})
+
+
 
 const PORT = process.env.PORT || 5000;
 
